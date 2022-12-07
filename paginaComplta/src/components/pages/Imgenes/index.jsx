6e57b02b7bb01a5664637db1af3024f690bb7a33
Facundo Ulises Vassallo <facu.vassallo@outlook.com>
2022-12-07 15:58:05 -0300

    avansando seccion 5 y img

---
+++ paginaComplta/src/components/pages/Imgenes/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Imagenes = () => {
+    return(
+        <div>
+            <section className="scinco" id="scinco">
+                <h6>Portfolio</h6>
+                <h3>Mis Proyectos Mas Recientes</h3>
+                <div className="ds5">
+                    <div className="dimgs5 d111">
+                        <img src={fotos} alt="" className="imgs5"/>
+                    </div>
+                    <div className="dimgs5 d222">
+                        <img src={fotos} alt="" className="imgs5"/>
+                    </div>
+                    <div className="dimgs5 d333">
+                        <img src={fotos} alt="" className="imgs5"/>
+                    </div>
+                    <div className="dimgs5 d444">
+                        <img src={fotos} alt="" className="imgs5"/>
+                    </div>
+                    <div className="dimgs5 d555">
+                        <img src={fotos} alt="" className="imgs5"/>
+                    </div>
+                    <div className="dimgs5 d666">
+                        <img src={fotos} alt="" className="imgs5"/>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export { Imagenes }
