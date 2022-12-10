3746a3150e5a1f1a2334ee580b810762a5c53d41
Facundo Ulises Vassallo <facu.vassallo@outlook.com>
2022-12-10 16:08:47 -0300

    seccion 5 terminada

--- paginaComplta/src/components/pages/Imgenes/index.jsx
+++ paginaComplta/src/components/pages/Imgenes/index.jsx
@@ -12,79 +12,76 @@ import './Imagenes.css'
 
 const Imagenes = () => {
 
-    function imagengrande (foto){
-        const num=Number(foto)
-        console.log(num)
-        if(num==1){
-            return fotos1
-        }else{
-            if(foto==2){
-                return fotos2
-            }else{
-                if(foto==3){
-                    return fotos3
-                }else{
-                    if(foto==4){
-                        return fotos4
-                    }else{
-                        if(foto==5){
-                            return fotos5
-                        }else{
-                            return fotos6
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
     
     return(
         
         <div className="imgcinco">
-            <div className='ful_img' id='fulImgBox'>
-                <img src={imagengrande()} id='fulImg' alt="" />
+            <div className='ful_img' id='fulImgBox1'>
+                <img src={fotos1} id='fulImg' alt="" />
                 <span onClick={()=>{
-                    fulImgBox.style.display= 'none'
+                    fulImgBox1.style.display= 'none'
+                }}>X</span>
+            </div>
+            <div className='ful_img' id='fulImgBox2'>
+                <img src={fotos2} id='fulImg' alt="" />
+                <span onClick={()=>{
+                    fulImgBox2.style.display= 'none'
+                }}>X</span>
+            </div>
+            <div className='ful_img' id='fulImgBox3'>
+                <img src={fotos3} id='fulImg' alt="" />
+                <span onClick={()=>{
+                    fulImgBox3.style.display= 'none'
+                }}>X</span>
+            </div>
+            <div className='ful_img' id='fulImgBox4'>
+                <img src={fotos4} id='fulImg' alt="" />
+                <span onClick={()=>{
+                    fulImgBox4.style.display= 'none'
+                }}>X</span>
+            </div>
+            <div className='ful_img' id='fulImgBox5'>
+                <img src={fotos5} id='fulImg' alt="" />
+                <span onClick={()=>{
+                    fulImgBox5.style.display= 'none'
+                }}>X</span>
+            </div>
+            <div className='ful_img' id='fulImgBox6'>
+                <img src={fotos6} id='fulImg' alt="" />
+                <span onClick={()=>{
+                    fulImgBox6.style.display= 'none'
                 }}>X</span>
             </div>   
             <div className="ds5">
                 <div className="dimgs5 d111">
                     <img src={fotos1} alt="" className="imgs5" onClick={()=>{
-                    fulImgBox.style.display= 'flex'
+                    fulImgBox1.style.display= 'flex'
                     
-                    imagengrande(1)
                 }}/>
                 </div>
                 <div className="dimgs5 d222">
                     <img src={fotos2} alt="" className="imgs5" onClick={()=>{  
-                    fulImgBox.style.display= 'flex'
-                    imagengrande(2)
-
+                    fulImgBox2.style.display= 'flex'
                 }}/>
                 </div>
                 <div className="dimgs5 d333">
                     <img src={fotos3} alt="" className="imgs5" onClick={()=>{ 
-                    fulImgBox.style.display= 'flex'
-                    imagengrande(3)
+                    fulImgBox3.style.display= 'flex'
                 }}/>
                 </div>
                 <div className="dimgs5 d444">
                     <img src={fotos4} alt="" className="imgs5" onClick={()=>{
-                    fulImgBox.style.display= 'flex'
-                    imagengrande(4)
+                    fulImgBox4.style.display= 'flex'
                 }}/>
                 </div>
                 <div className="dimgs5 d555">
                     <img src={fotos5} alt="" className="imgs5" onClick={()=>{
-                    fulImgBox.style.display= 'flex'
-                    imagengrande(5)
+                    fulImgBox5.style.display= 'flex'
                 }}/>
                 </div>
                 <div className="dimgs5 d666">
                     <img src={fotos6} alt="" className="imgs5" onClick={()=>{
-                    fulImgBox.style.display= 'flex'
-                    imagengrande(6)
+                    fulImgBox6.style.display= 'flex'
                 }}/>
                 </div>
             </div>
